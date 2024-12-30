c870a89ff8c48e93cecb6fb373c4360e672ec984
h0bbl3s <h0bbl3s@yahoo.com>
2024-12-30 09:40:49 -0500

    tweak

--- src/Example_08B/Example_08B.ino
+++ src/Example_08B/Example_08B.ino
@@ -2,18 +2,18 @@
 //
 // Copy and paste this example into an empty Arduino sketch
 
-#define SENSOR A0   
+#define SENSOR A0
 #define R_LED 9
 #define G_LED 10
 #define B_LED 11
 #define BUTTON 12
 
-int val = 0; // variable to store the value coming from the sensor
+int val = 0;  // variable to store the value coming from the sensor
 
 int btn = LOW;
 int old_btn = LOW;
 int state = 0;
-char buffer[7] ;
+char buffer[7];
 int pointer = 0;
 byte inByte = 0;
 
@@ -24,17 +24,17 @@ byte b = 0;
 void setup() {
   Serial.begin(9600);  // open the serial port
   pinMode(BUTTON, INPUT);
-  pinMode(R_LED, OUTPUT); // used for PWM
-  pinMode(G_LED, OUTPUT); // used for PWM
-  pinMode(B_LED, OUTPUT); // used for PWM
+  pinMode(R_LED, OUTPUT);  // used for PWM
+  pinMode(G_LED, OUTPUT);  // used for PWM
+  pinMode(B_LED, OUTPUT);  // used for PWM
 }
 
 void loop() {
-  val = analogRead(SENSOR); // read the value from the sensor
-  Serial.println(val);      // print the value to
-                            // the serial port
+  val = analogRead(SENSOR);  // read the value from the sensor
+  Serial.println(val);       // print the value to
+                             // the serial port
 
-  if (Serial.available() >0) {
+  if (Serial.available() > 0) {
 
     // read the incoming byte:
     inByte = Serial.read();
@@ -42,9 +42,9 @@ void loop() {
     // If the marker's found, next 6 characters are the colour
     if (inByte == '#') {
 
-      while (pointer < 6) { // accumulate 6 chars
-        buffer[pointer] = Serial.read(); // store in the buffer
-        pointer++; // move the pointer forward by 1
+      while (pointer < 6) {               // accumulate 6 chars
+        buffer[pointer] = Serial.read();  // store in the buffer
+        pointer++;                        // move the pointer forward by 1
       }
 
       // now we have the 3 numbers stored as hex numbers
@@ -52,40 +52,39 @@ void loop() {
       r = hex2dec(buffer[1]) + hex2dec(buffer[0]) * 16;
       g = hex2dec(buffer[3]) + hex2dec(buffer[2]) * 16;
       b = hex2dec(buffer[5]) + hex2dec(buffer[4]) * 16;
-      
-      pointer = 0; // reset the pointer so we can reuse the buffer
-     
-    }
-  }   
 
-  btn = digitalRead(BUTTON); // read input value and store it
+      pointer = 0;  // reset the pointer so we can reuse the buffer
+    }
+  }
+
+  btn = digitalRead(BUTTON);  // read input value and store it
 
   // Check if there was a transition
-  if ((btn == HIGH) && (old_btn == LOW)){
+  if ((btn == HIGH) && (old_btn == LOW)) {
     state = 1 - state;
   }
 
-  old_btn = btn; // val is now old, let's store it
+  old_btn = btn;  // val is now old, let's store it
 
-  if (state == 1) { // if the lamp is on
+  if (state == 1) {  // if the lamp is on
 
-    analogWrite(R_LED, r);  // turn the leds on
-    analogWrite(G_LED, g);  // at the colour
-    analogWrite(B_LED, b);  // sent by the computer
+    analogWrite(R_LED, r);
+    analogWrite(G_LED, g);
+    analogWrite(B_LED, b);
   } else {
 
     analogWrite(R_LED, 0);  // otherwise turn off
-    analogWrite(G_LED, 0); 
+    analogWrite(G_LED, 0);
     analogWrite(B_LED, 0);
-   }
- 
-  delay(100);                // wait 100ms between each send
+  }
+
+  delay(100);  // wait 100ms between each send
 }
 
-int hex2dec(byte c) { // converts one HEX character into a number
-    if (c >= '0' && c <= '9') {
-      return c - '0';
-    } else if (c >= 'A' && c <= 'F') {
-      return c - 'A' + 10;
-    }
+int hex2dec(byte c) {  // converts one HEX character into a number
+  if (c >= '0' && c <= '9') {
+    return c - '0';
+  } else if (c >= 'A' && c <= 'F') {
+    return c - 'A' + 10;
+  }
 }
